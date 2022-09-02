123d5269cae777ecc4e5ceeac3bda10b226aba55
milene <rinvysson.dev@hotmail.com>
2022-09-02 11:18:23 -0300

    ..

--- database/database.js
+++ database/database.js
@@ -1,8 +1,12 @@
 const Sequelize = require("sequelize");
-const senha = 123456;
+
+// Autenticação
+const NomedaTabela = "digite o nome da tabela";
+const login = "digite o login do db";
+const senha = "digite a senha";
 
 // Conectando com o banco de dados | nome | usuario | senha
-const connection = new Sequelize("guiaperguntas", "root", senha, {
+const connection = new Sequelize(NomedaTabela, login, senha, {
   host: "localhost",
   dialect: "mysql",
 });
